f050a04885bf4fb3e854eddf41e640363c11a550
John Sanpe <sanpeqf@gmail.com>
2022-08-19 08:44:16 +0800

    Function: Added shortest path find/insert func.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- src/skiplist.c
+++ src/skiplist.c
@@ -6,6 +6,19 @@
 #include "skiplist.h"
 #include <stdlib.h>
 
+static unsigned int random_level(struct skip_head *head)
+{
+    unsigned int level = 1;
+
+    do {
+        if (rand() < RAND_MAX >> 2)
+            break;
+        level++;
+    } while (level < head->levels);
+
+    return level;
+}
+
 static struct skip_node *
 skipnode_find(struct skip_head *head, const void *key,
               skiplist_find_t find, unsigned int *plev)
@@ -15,12 +28,14 @@ skipnode_find(struct skip_head *head, const void *key,
     struct list_head *list;
     long retval;
 
+    if (unlikely(!level))
+        return NULL;
+
     list = &head->nodes[level - 1];
-    match = list_first_entry_or_null(list, struct skip_node, list[level - 1]);
+    match = list_first_entry(list, struct skip_node, list[level - 1]);
+    retval = find(match->pdata, key);
 
-    for (; match && level--; --list) {
-        retval = find(match->pdata, key);
-
+    for (; level--; --list) {
         if (retval < 0) {
             walk = match;
             list_for_each_entry_continue(walk, list, list[level]) {
@@ -51,19 +66,6 @@ skipnode_find(struct skip_head *head, const void *key,
     return NULL;
 }
 
-static unsigned int random_level(struct skip_head *head)
-{
-    unsigned int level = 1;
-
-    do {
-        if (rand() < RAND_MAX >> 2)
-            break;
-        level++;
-    } while (level < head->levels);
-
-    return level;
-}
-
 int skiplist_insert(struct skip_head *head, void *data, skiplist_cmp_t cmp)
 {
     struct skip_node *walk, *match, *node;
@@ -84,13 +86,13 @@ int skiplist_insert(struct skip_head *head, void *data, skiplist_cmp_t cmp)
     for (; unlikely(list_check_empty(list)); --list) {
         list_add(list, &node->list[--level]);
         if (!level)
-            return 0;
+            goto finish;
     }
 
     match = list_first_entry(list, struct skip_node, list[level - 1]);
-    for (; level--; --list) {
-        retval = cmp(data, match->pdata);
+    retval = cmp(data, match->pdata);
 
+    for (; level--; --list) {
         if (retval > 0) {
             walk = match;
             list_for_each_entry_continue(walk, list, list[level]) {
@@ -117,6 +119,128 @@ int skiplist_insert(struct skip_head *head, void *data, skiplist_cmp_t cmp)
             list_add_prev(&match->list[level], &node->list[level]);
     }
 
+finish:
+    head->count++;
+    return 0;
+}
+
+static struct skip_node *
+skipnode_even_find(struct skip_head *head, const void *key,
+                   skiplist_even_find_t find, unsigned int *plev)
+{
+    unsigned int level = head->curr;
+    struct skip_node *walk, *match;
+    struct list_head *list;
+    unsigned long disparity, compare;
+    bool signplus;
+
+    if (unlikely(!level))
+        return NULL;
+
+    list = &head->nodes[level - 1];
+    match = list_first_entry(list, struct skip_node, list[level - 1]);
+    disparity = find(match->pdata, key, &signplus);
+
+    for (; level--; --list) {
+        if (disparity) {
+            if ((walk = list_next_entry_or_null(match, list, list[level]))) {
+                list_for_each_entry_from(walk, list, list[level]) {
+                    compare = find(walk->pdata, key, &signplus);
+                    if (disparity <= compare &&
+                        (disparity == compare && signplus != false))
+                        break;
+                    disparity = compare;
+                    match = walk;
+                    if (!disparity)
+                        goto finish;
+                }
+            }
+
+            if ((walk = list_prev_entry_or_null(match, list, list[level]))) {
+                list_for_each_entry_reverse_from(walk, list, list[level]) {
+                    compare = find(walk->pdata, key, &signplus);
+                    if (disparity >= compare &&
+                        (disparity == compare && signplus != true))
+                        break;
+                    disparity = compare;
+                    match = walk;
+                    if (!disparity)
+                        goto finish;
+                }
+            }
+        }
+
+finish:
+        if (disparity == 0) {
+            if (plev)
+                *plev = level + 1;
+            return match;
+        }
+    }
+
+    return NULL;
+}
+
+int skiplist_even_insert(struct skip_head *head, void *data, skiplist_even_cmp_t cmp)
+{
+    struct skip_node *walk, *match, *node;
+    struct list_head *list;
+    unsigned int level;
+    unsigned long disparity, compare;
+    bool signplus;
+
+    level = random_level(head);
+    head->curr = head->curr > level ? head->curr : level;
+
+    node = malloc(sizeof(*node) + sizeof(*node->list) * level);
+    if (unlikely(!node))
+        return -ENOMEM;
+
+    node->pdata = data;
+    list = &head->nodes[level - 1];
+
+    for (; unlikely(list_check_empty(list)); --list) {
+        list_add(list, &node->list[--level]);
+        if (!level)
+            goto finish;
+    }
+
+    match = list_first_entry(list, struct skip_node, list[level - 1]);
+    disparity = cmp(data, match->pdata, &signplus);
+
+    for (; level--; --list) {
+        if (disparity) {
+            if ((walk = list_next_entry_or_null(match, list, list[level]))) {
+                list_for_each_entry_from(walk, list, list[level]) {
+                    compare = cmp(node->pdata, walk->pdata, &signplus);
+                    if (disparity <= compare &&
+                        (disparity == compare && signplus != true))
+                        break;
+                    disparity = compare;
+                    match = walk;
+                }
+            }
+
+            if ((walk = list_prev_entry_or_null(match, list, list[level]))) {
+                list_for_each_entry_reverse_from(walk, list, list[level]) {
+                    compare = cmp(node->pdata, walk->pdata, &signplus);
+                    if (disparity <= compare &&
+                        (disparity == compare && signplus != false))
+                        break;
+                    disparity = compare;
+                    match = walk;
+                }
+            }
+        }
+
+        if (signplus)
+            list_add(&match->list[level], &node->list[level]);
+        else
+            list_add_prev(&match->list[level], &node->list[level]);
+    }
+
+finish:
+    head->count++;
     return 0;
 }
 
@@ -129,9 +253,13 @@ void skiplist_delete(struct skip_head *head, void *key, skiplist_find_t find)
     if (unlikely(!node))
         return;
 
-    while (level--)
+    while (level--) {
         list_del(&node->list[level]);
+        if (list_check_empty(&head->nodes[level]))
+            head->curr = level;
+    }
 
+    head->count--;
     free(node);
 }
 
@@ -139,6 +267,32 @@ void *skiplist_find(struct skip_head *head, void *key, skiplist_find_t find)
 {
     struct skip_node *node;
     node = skipnode_find(head, key, find, NULL);
+    return node ? node->pdata : NULL;
+}
+
+void skiplist_even_delete(struct skip_head *head, void *key, skiplist_even_find_t find)
+{
+    struct skip_node *node;
+    unsigned int level;
+
+    node = skipnode_even_find(head, key, find, &level);
+    if (unlikely(!node))
+        return;
+
+    while (level--) {
+        list_del(&node->list[level]);
+        if (list_check_empty(&head->nodes[level]))
+            head->curr = level;
+    }
+
+    head->count--;
+    free(node);
+}
+
+void *skiplist_even_find(struct skip_head *head, void *key, skiplist_even_find_t find)
+{
+    struct skip_node *node;
+    node = skipnode_even_find(head, key, find, NULL);
     return node ? node->pdata : NULL;
 }
 
